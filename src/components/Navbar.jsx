2968571fb3b6831b681ad8d67a6c80d83f1610f5
Diku Mardi <dikumardi047@gmail.com>
2025-12-23 21:47:21 +0530

    added Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import smiley from '../assets/smiley.svg'
 const Navbar = () => {
   return (
     <div className="hidden lg:flex bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 fixed z-50">
-      <div className="w-full lg:w-[500px] h-[49.3px] bg-black rounded-full flex items-center justify-center gap-3 text-white py-2 px-4">
+      <div className="w-full lg:w-125 h-[49.3px] bg-black rounded-full flex items-center justify-center gap-3 text-white py-2 px-4">
         <div className="logo">
           <img src={smiley} alt="" className="w-6 h-6"/>
         </div>
